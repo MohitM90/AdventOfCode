06da40f19342cf1a053027f2888afe7922600ba5
Mohit Makhija <mohit@outlook.de>
2024-12-02 10:25:03 +0100

    Day 3 Template

---
+++ Advent-of-Code-2024/AdventOfCode2024/AdventOfCode2024/Day03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+
+internal class Day03 : BaseDay
+{
+    public override int Puzzle1()
+    {
+        int answer = 0;
+        var inputs = Input.Split("\r\n");
+
+
+
+        return answer;
+    }
+
+    public override int Puzzle2()
+    {
+        int answer = 0;
+        var inputs = Input.Split("\r\n");
+
+
+
+        return answer;
+    }
+}
