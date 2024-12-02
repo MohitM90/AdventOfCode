7adf7404b63601bbfb7606c055d1b58d559728a0
Mohit Makhija <mohit@outlook.de>
2024-12-02 09:09:53 +0100

    Day 2 Puzzle 1

--- Advent-of-Code-2024/AdventOfCode2024/AdventOfCode2024/Day02.cs
+++ Advent-of-Code-2024/AdventOfCode2024/AdventOfCode2024/Day02.cs
@@ -10,11 +10,68 @@ internal class Day02 : BaseDay
 {
     public override int Puzzle1()
     {
-        return 0;
+        int answer = 0;
+        var inputs = Input
+            .Split("\r\n")
+            .Select(x => x.Split(" ")
+                .Select(s => int.Parse(s))
+                .ToArray());
+
+        foreach (var report in inputs)
+        {
+            if (IsSafe(report))
+            {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 
     public override int Puzzle2()
     {
         return 0;
+    }
+
+    private bool IsSafe(int[] report) {
+        bool? isDecreasing = null;
+        int start = report[0];
+        for (int i = 1; i < report.Length; i++)
+        {
+            if (i == 1)
+            {
+                if (report[i] < start)
+                {
+                    isDecreasing = true;
+                }
+                else if (report[i] > start)
+                {
+                    isDecreasing = false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            var prev = report[i - 1];
+            var current = report[i];
+            if (isDecreasing == true && current >= prev)
+            {
+                return false;
+            }
+            if (isDecreasing == false && current <= prev)
+            {
+                return false;
+            }
+
+            var diff = Math.Abs(prev - current);
+            if (diff < 1 || diff > 3)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- Advent-of-Code-2024/AdventOfCode2024/AdventOfCode2024/Program.cs
+++ Advent-of-Code-2024/AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode2024;
 
-new Day01().Run();
+new Day02().Run();
