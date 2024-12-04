e3be8676961512d778c7b64b469c6c08ff6d17c6
Mohit Makhija <mohit@outlook.de>
2024-12-04 08:19:47 +0100

    Day 4 Draft

---
+++ Advent-of-Code-2024/AdventOfCode2024/Day04.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+
+internal class Day04 : BaseDay
+{
+    public override int Puzzle1()
+    {
+        int answer = 0;
+        var input = Input.Split("\r\n");
+
+        answer += CountStringHorizontal(input, "XMAS|SAMX");
+        answer += CountStringDiagonal(input, "XMAS");
+        answer += CountStringDiagonal(input, "SAMX");
+
+        return answer;
+    }
+
+    public override int Puzzle2()
+    {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    private int CountStringHorizontal(string[] input, string pattern)
+    {
+        return input.Sum(s =>
+        {
+            return Regex.Matches(s, pattern).Count;
+        });
+    }
+
+    private int CountStringDiagonal(string[] input, string search)
+    {
+        int count = 0;
+        for (int i = 0; i < input.Length - search.Length; i++)
+        {
+            for (int j = 0; j < input[i].Length - search.Length; j++)
+            {
+                string s = "";
+                for (int k = j; k < j + search.Length; k++)
+                {
+                    s += input[i + k][k];
+                }
+                if (s == search)
+                {
+                    count++;
+                }
+            }
+            
+        }
+        return count;
+    }
+}
--- Advent-of-Code-2024/AdventOfCode2024/Program.cs
+++ Advent-of-Code-2024/AdventOfCode2024/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode2024;
 
-new Day03().Run();
+new Day04().Run();
