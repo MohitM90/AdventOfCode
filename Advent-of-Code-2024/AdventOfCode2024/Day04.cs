5a87948c229f40863e715ac1247e053c87ed8cc3
Mohit Makhija <mohit.makhija@avanade.com>
2024-12-04 13:34:36 +0100

    Day 4 Puzzle 2

--- Advent-of-Code-2024/AdventOfCode2024/Day04.cs
+++ Advent-of-Code-2024/AdventOfCode2024/Day04.cs
@@ -14,18 +14,20 @@ internal class Day04 : BaseDay
         int answer = 0;
         var input = Input.Split("\r\n");
 
-        answer += CountStringHorizontal(input, "XMAS");
-        answer += CountStringHorizontal(Transpose(input), "XMAS");
-        answer += CountStringDiagonal(input, "XMAS");
-        answer += CountStringDiagonal(Reverse(input), "XMAS");
+        string search = "XMAS";
+        answer += CountStringHorizontal(input, search);
+        answer += CountStringHorizontal(Transpose(input), search);
+        answer += CountStringDiagonal(input, search);
+        answer += CountStringDiagonal(Reverse(input), search);
         return answer;
     }
 
     public override int Puzzle2()
     {
         int answer = 0;
-        
+        var input = Input.Split("\r\n");
 
+        answer = CountStringCrossDiagonal(input, "MAS");
 
         return answer;
     }
@@ -58,6 +60,38 @@ internal class Day04 : BaseDay
         }
         return count;
     }
+    private int CountStringCrossDiagonal(string[] input, string search)
+    {
+        int count = 0;
+        for (int i = 0; i < input.Length - search.Length + 1; i++)
+        {
+            for (int j = 0; j < input[i].Length - search.Length + 1; j++)
+            {
+                bool left = ContainsDiagonal(input, j, i, search, Direction.Forward);
+                bool right = ContainsDiagonal(input, j + (search.Length - 1), i, search, Direction.Backward);
+                string searchReverse = new string(search.Reverse().ToArray());
+                bool leftReverse = ContainsDiagonal(input, j, i, searchReverse, Direction.Forward);
+                bool rightReverse = ContainsDiagonal(input, j + (search.Length - 1), i, searchReverse, Direction.Backward);
+                if ((left || leftReverse) && (right || rightReverse))
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private bool ContainsDiagonal(string[] input, int startX, int startY, string search, Direction direction)
+    {
+        for (int i = 0; i < search.Length; i++)
+        {
+            if (input[startY + i][startX + i * (int)direction] != search[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
     private string[] Transpose(string[] input)
     {
@@ -81,5 +115,11 @@ internal class Day04 : BaseDay
             output[i] = new string(input[i].Reverse().ToArray());
         }
         return output;
+    }
+
+    private enum Direction
+    {
+        Forward = 1,
+        Backward = -1
     }
 }
