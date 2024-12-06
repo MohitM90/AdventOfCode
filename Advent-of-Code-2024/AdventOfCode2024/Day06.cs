f1c4746eb2c48491501d0ab8ef4855cc120358ec
Mohit Makhija <mohit@outlook.de>
2024-12-06 09:30:53 +0100

    Day 6 Template

---
+++ Advent-of-Code-2024/AdventOfCode2024/Day06.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+
+internal class Day06 : BaseDay
+{
+    public override int Puzzle1()
+    {
+        int answer = 0;
+        
+
+        return answer;
+    }
+
+    public override int Puzzle2()
+    {
+        int answer = 0;
+        
+
+
+
+        return answer;
+    }
+
+}
