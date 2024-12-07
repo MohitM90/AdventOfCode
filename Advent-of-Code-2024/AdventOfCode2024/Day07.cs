261fd2fe7ccad74f331781fbce2e10ca37fd2d50
Mohit Makhija <mohit@outlook.de>
2024-12-07 15:49:29 +0100

    Day 7 Puzzle 2

--- Advent-of-Code-2024/AdventOfCode2024/Day07.cs
+++ Advent-of-Code-2024/AdventOfCode2024/Day07.cs
@@ -28,47 +28,70 @@ internal class Day07 : BaseDay
     {
         long answer = 0;
         var inputs = Input.Split("\r\n");
+        var calibration = inputs
+    .Select(x => x.Split([": ", " "], StringSplitOptions.None))
+    .Select(s => new Calibration(
+        long.Parse(s[0]),
+        s[1..].Select(l => long.Parse(l)).ToArray()));
 
-        
-
+        answer = calibration.Sum(c => Solve(c.Sum, c.Inputs.First(), c.Inputs[1..], true));
 
         return answer;
     }
 
-    private long Solve(long sum, long initial, long[] inputs)
+    private long Solve(long sum, long initial, long[] inputs, bool concatEnabled = false)
     {
-        if (Solve(sum, initial, inputs, Operator.Mul) == sum)
+        if (Solve(sum, initial, inputs, Operator.Mul, concatEnabled) == (sum, true))
         {
             return sum;
         }
         return 0;
     }
 
-    private long Solve(long sum, long initial, long[] inputs, Operator op)
+    private (long, bool) Solve(long sum, long initial, long[] inputs, Operator op, bool concatEnabled = false)
     {
         if (inputs.Length == 0)
         {
-            return initial;
+            return (initial, true);
         }
+
+        bool reachedEnd = false;
 
         var temp = op switch
         {
             Operator.Mul => initial * inputs[0],
             Operator.Add => initial + inputs[0],
+            Operator.Concat => Concat(initial, inputs[0]),
             _ => throw new NotImplementedException()
         };
 
+
         if (temp < sum)
         {
-            temp = Solve(sum, temp, inputs[1..], Operator.Mul);
+            (temp, reachedEnd) = Solve(sum, temp, inputs[1..], Operator.Mul, concatEnabled);
         }
 
         if (temp != sum && op == Operator.Mul)
         {
-            temp = Solve(sum, initial, inputs, Operator.Add);
+            (temp, reachedEnd) = Solve(sum, initial, inputs, Operator.Add, concatEnabled);
         }
 
-        return temp;
+        if (concatEnabled && temp != sum && op == Operator.Add)
+        {
+            (temp, reachedEnd) = Solve(sum, initial, inputs, Operator.Concat, concatEnabled);
+        }
+
+        if (reachedEnd)
+        {
+            return (temp, true);
+        }
+
+        return Solve(sum, temp, inputs[1..], Operator.Mul, concatEnabled);
+    }
+
+    private long Concat(long p, long q)
+    {
+        return p * (long)Math.Pow(10, (int)Math.Log10(q) + 1) + q;
     }
 
     private record Calibration(long Sum, long[] Inputs);
@@ -76,6 +99,7 @@ internal class Day07 : BaseDay
     private enum Operator
     {
         Mul,
-        Add
+        Add,
+        Concat
     }
 }
