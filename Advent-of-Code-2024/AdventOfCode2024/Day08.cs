6da5538c3906c1a75b2d74b56ee977dfd1a27acd
Mohit Makhija <mohit@outlook.de>
2024-12-08 09:03:24 +0100

    Day 8 Template

---
+++ Advent-of-Code-2024/AdventOfCode2024/Day08.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+
+internal class Day08 : BaseDay
+{
+    public override long Puzzle1()
+    {
+        long answer = 0;
+        var inputs = Input.Split("\r\n");
+        
+
+        return answer;
+    }
+
+    public override long Puzzle2()
+    {
+        long answer = 0;
+        var inputs = Input.Split("\r\n");
+        
+
+        return answer;
+    }
+
+    
+}
