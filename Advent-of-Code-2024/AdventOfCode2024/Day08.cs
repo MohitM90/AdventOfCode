d6e2f8785c618c943afb60195211edc1aa25706d
Mohit Makhija <mohit@outlook.de>
2024-12-08 10:31:40 +0100

    Day 8 Puzzle 1

--- Advent-of-Code-2024/AdventOfCode2024/Day08.cs
+++ Advent-of-Code-2024/AdventOfCode2024/Day08.cs
@@ -7,13 +7,20 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2024;
 
-internal class Day08 : BaseDay
+internal partial class Day08 : BaseDay
 {
     public override long Puzzle1()
     {
         long answer = 0;
         var inputs = Input.Split("\r\n");
-        
+        ILookup<char, Point> antennas = GetAntennaPositions(inputs);
+        char[,] antinodeMap = new char[inputs[0].Length, inputs.Length];
+        foreach (var antenna in antennas)
+        {
+            MarkAntinodes(inputs, antenna.Key, [.. antenna], antinodeMap);
+        }
+
+        answer = antinodeMap.Cast<char>().Count(c => c == '#');
 
         return answer;
     }
@@ -27,5 +34,56 @@ internal class Day08 : BaseDay
         return answer;
     }
 
-    
+
+    private static ILookup<char, Point> GetAntennaPositions(string[] inputs)
+    {
+        return inputs.SelectMany((s, y) => AntennaRegex().Matches(s)
+            .Select(m => new { Antenna = m.Value[0], Position = new Point(m.Index, y) }))
+            .ToLookup(x => x.Antenna, x => x.Position);
+    }
+
+    private static void MarkAntinodes(string[] map, char antenna, IList<Point> points, char[,] antinodeMap)
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            var p1 = points[i];
+            for (int j = i + 1; j < points.Count; j++)
+            {
+                var p2 = points[j];
+                var dx = p2.X - p1.X;
+                var dy = p2.Y - p1.Y;
+
+                var node1 = Point.MinY(p1, p2);
+                var node2 = Point.MaxY(p1, p2);
+                if (node1 == node2)
+                {
+                    node1 = Point.MinX(p1, p2);
+                    node2 = Point.MaxX(p1, p2);
+                }
+
+                var antinode1 = new Point(node1.X - dx, node1.Y - dy);
+                var antinode2 = new Point(node2.X + dx, node2.Y + dy);
+
+                if (antinode1.X >= 0 && antinode1.X < map[0].Length && antinode1.Y >= 0 && antinode1.Y < map.Length)
+                {
+                    antinodeMap[antinode1.X, antinode1.Y] = '#';
+                }
+                if (antinode2.X >= 0 && antinode2.X < map[0].Length && antinode2.Y >= 0 && antinode2.Y < map.Length)
+                {
+                    antinodeMap[antinode2.X, antinode2.Y] = '#';
+                }
+            }
+        }
+    }
+
+    private record Point(int X, int Y)
+    {
+        public static Point MinY(Point p1, Point p2) => p1.Y < p2.Y ? p1 : p2;
+        public static Point MaxY(Point p1, Point p2) => p1.Y > p2.Y ? p1 : p2;
+        public static Point MinX(Point p1, Point p2) => p1.X < p2.X ? p1 : p2;
+        public static Point MaxX(Point p1, Point p2) => p1.X > p2.X ? p1 : p2;
+    }
+
+    [GeneratedRegex("[A-Za-z0-9]")]
+    private static partial Regex AntennaRegex();
 }
--- Advent-of-Code-2024/AdventOfCode2024/Program.cs
+++ Advent-of-Code-2024/AdventOfCode2024/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AdventOfCode2024;
 
 
-new Day07().Run();
+new Day08().Run();
 Console.ReadKey();
