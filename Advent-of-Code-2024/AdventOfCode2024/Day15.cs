690781001b63a84896fa993a7d6dd2305787ba9b
Mohit Makhija <mohit@outlook.de>
2024-12-14 18:52:07 +0100

    Day 15 Template

---
+++ Advent-of-Code-2024/AdventOfCode2024/Day15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+internal class Day15 : BaseDay
+{
+
+    public override long Puzzle1()
+    {
+        long answer = 0;
+        var inputs = Input.Split("\r\n");
+
+
+        return answer;
+    }
+
+    public override long Puzzle2()
+    {
+        long answer = 0;
+        var inputs = Input.Split("\r\n");
+
+
+        return answer;
+    }
+
+}
