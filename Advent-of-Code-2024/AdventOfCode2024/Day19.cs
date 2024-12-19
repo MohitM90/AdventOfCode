4bdeeec5742bcb417965e65eb260b79a9eac84d4
Mohit Makhija <mohit.makhija@avanade.com>
2024-12-19 14:43:53 +0100

    Day 19 Template

---
+++ Advent-of-Code-2024/AdventOfCode2024/Day19.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+internal class Day19 : BaseDay<long>
+{
+
+    public override long Puzzle1()
+    {
+        long answer = 0;
+        var input = Input.Split("\r\n").Select(x => x.Split(",").Select(y => int.Parse(y)).ToArray());
+
+
+        return answer;
+    }
+
+    public override long Puzzle2()
+    {
+        long answer = 0;
+        var input = Input.Split("\r\n").Select(x => x.Split(",").Select(y => int.Parse(y)).ToArray());
+
+
+        return answer;
+    }
+
+}
