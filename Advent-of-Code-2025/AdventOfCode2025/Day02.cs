a475eef276d57a7b15f954f710670d3516ebcdd0
Mohit Makhija <mohit.makhija@avanade.com>
2025-12-03 11:31:52 +0100

    Day 02

--- Advent-of-Code-2025/AdventOfCode2025/Day02.cs
+++ Advent-of-Code-2025/AdventOfCode2025/Day02.cs
@@ -9,9 +9,39 @@ internal class Day02 : BaseDay<long>
     public override async Task<long> Puzzle1(string input)
     {
         long answer = 0;
-        var inputs = input.Split("\r\n");
+        var inputs = input.Split(",");
+        foreach (var i in inputs)
+        {
+            var range = i.Split("-");
+            var minLen = range[0].Length;
+            var maxLen = range[1].Length;
 
+            if (minLen % 2 == 1 && maxLen % 2 == 1)
+            {
+                continue;
+            }
 
+            var len = (minLen % 2 == 1 ? maxLen : minLen) / 2;
+
+            long min = long.Parse(range[0]);
+
+            var max = long.Parse(range[1]);
+            var minUpper = minLen % 2 == 1 && len > 1 ? (long)Math.Max(long.Parse(range[0][..(len - 1)]), Math.Pow(10, len-1)) : long.Parse(range[0][..len]);
+
+            long b = (long)Math.Pow(10, len);
+
+            long n = minUpper;
+            while (n < b && n * b + n <= max)
+            {
+                long id = n * b + n;
+                if (id >= min)
+                {
+                    answer += id;
+                }
+                
+                n++;
+            }
+        }
 
         return answer;
     }
@@ -19,9 +49,57 @@ internal class Day02 : BaseDay<long>
     public override async Task<long> Puzzle2(string input)
     {
         long answer = 0;
-        var inputs = input.Split("\r\n");
+        var inputs = input.Split(",");
+        foreach (var i in inputs)
+        {
+            var range = i.Split("-");
+            var minLen = range[0].Length;
+            var maxLen = range[1].Length;
 
+            long min = long.Parse(range[0]);
+            long max = long.Parse(range[1]);
 
+            int n = 1;
+            var answers = new HashSet<long>();
+            while (n <= maxLen/2)
+            {
+                for (int x = (int)Math.Pow(10, n - 1); x <= (int)Math.Pow(10, n); x++)
+                {
+                    var substr = x.ToString()[..n].Repeat(minLen/n);
+                    long val = long.Parse(substr);
+
+                    if (val > max)
+                    {
+                        break;
+                    }
+
+                    if (val >= min && val <= max)
+                    {
+                        answers.Add(val);
+                    }
+
+                    if (minLen != maxLen)
+                    {
+                        substr = x.ToString()[..n].Repeat(maxLen / n);
+                        val = long.Parse(substr);
+                        if (val >= min && val <= max)
+                        {
+                            answers.Add(val);
+                        }
+                    }
+                }
+                n++;
+            }
+            foreach (var a in answers)
+            {
+                if (a < 10)
+                {
+                    continue;
+                }
+                //Console.WriteLine(a);
+                answer += a;
+            }
+        }
 
         return answer;
     }
--- Advent-of-Code-2025/AdventOfCode2025/Extensions.cs
+++ Advent-of-Code-2025/AdventOfCode2025/Extensions.cs
@@ -16,4 +16,16 @@ internal static class Extensions
     {
         return (Math.Abs(a * b) + a) % b;
     }
+
+    public static string Repeat(this string input, int count)
+    {
+        if (string.IsNullOrEmpty(input) || count <= 1)
+            return input;
+
+        var builder = new StringBuilder(input.Length * count);
+
+        for (var i = 0; i < count; i++) builder.Append(input);
+
+        return builder.ToString();
+    }
 }
