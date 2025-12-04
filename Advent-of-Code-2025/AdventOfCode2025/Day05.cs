27f192220a3937ce2d504a52366313ba91c0ad62
Mohit Makhija <mohit.makhija@avanade.com>
2025-12-04 10:44:41 +0100

    Day 05 Skeleton

---
+++ Advent-of-Code-2025/AdventOfCode2025/Day05.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2025;
+
+internal class Day05 : BaseDay<long>
+{
+    public override async Task<long> Puzzle1(string input)
+    {
+        long answer = 0;
+        var inputs = input.Split("\r\n");
+
+
+
+        return answer;
+    }
+
+    public override async Task<long> Puzzle2(string input)
+    {
+        long answer = 0;
+        var inputs = input.Split("\r\n");
+
+
+
+        return answer;
+    }
+}
