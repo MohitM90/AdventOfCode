23aa4a5d78917259e94714ae3b4187f7055e1f40
Mohit Makhija <mohit@outlook.de>
2025-12-05 06:37:00 +0100

    Day 05 Puzzle 1

--- Advent-of-Code-2025/AdventOfCode2025/Day05.cs
+++ Advent-of-Code-2025/AdventOfCode2025/Day05.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Text;
 
 namespace AdventOfCode2025;
 
-internal class Day05 : BaseDay<long>
+internal class Day05 : BaseDay<ulong>
 {
-    public override async Task<long> Puzzle1(string input)
+    public override async Task<ulong> Puzzle1(string input)
     {
-        long answer = 0;
-        var inputs = input.Split("\r\n");
-
+        ulong answer = 0;
+        var inputs = input.Split("\r\n\r\n");
+        var ranges = inputs[0].Split("\r\n").Select(x => 
+        {
+            var minMax = x.Split("-");
+            var min = ulong.Parse(minMax[0]);
+            var max = ulong.Parse(minMax[1]);
+            return new ulong[] { min, max };
+        });
+        var ids = inputs[1].Split("\r\n").Select(x => ulong.Parse(x));
+        
+        foreach ( var id in ids )
+        {
+            foreach (var range in ranges)
+            {
+                var min = range[0];
+                var max = range[1];
+                
+                if (id >= min && id <= max)
+                {
+                    answer++; break;
+                }
+            }
+        }
 
 
         return answer;
     }
 
-    public override async Task<long> Puzzle2(string input)
+    public override async Task<ulong> Puzzle2(string input)
     {
-        long answer = 0;
-        var inputs = input.Split("\r\n");
+        ulong answer = 0;
+        var inputs = input.Split("\r\n\r\n");
 
 
 
--- Advent-of-Code-2025/AdventOfCode2025/Program.cs
+++ Advent-of-Code-2025/AdventOfCode2025/Program.cs
@@ -2,5 +2,5 @@
 
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-await new Day04().RunAsync();
+await new Day05().RunAsync();
 Console.ReadKey();
