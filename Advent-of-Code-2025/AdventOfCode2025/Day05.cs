a8c41f504e87e48244030427e86af2232cbe4912
Mohit Makhija <mohit@outlook.de>
2025-12-06 06:24:10 +0100

    Day 05 Puzzle 2

--- Advent-of-Code-2025/AdventOfCode2025/Day05.cs
+++ Advent-of-Code-2025/AdventOfCode2025/Day05.cs
@@ -42,8 +42,67 @@ internal class Day05 : BaseDay<ulong>
     {
         ulong answer = 0;
         var inputs = input.Split("\r\n\r\n");
+        var ranges = inputs[0].Split("\r\n").Select(x =>
+        {
+            var minMax = x.Split("-");
+            var min = ulong.Parse(minMax[0]);
+            var max = ulong.Parse(minMax[1]);
+            return (min, max);
+        });
+        var ids = inputs[1].Split("\r\n").Select(x => ulong.Parse(x));
 
+        var mergedRanges = new List<(ulong min, ulong max)>();
+        foreach (var range in ranges.OrderBy(x => x.min))
+        {
+            if (mergedRanges.Count == 0)
+            {
+                mergedRanges.Add(range);
+                continue;
+            }
 
+            bool isMerged = false;
+            for (int i = 0; i < mergedRanges.Count; i++)
+            {
+                var mergedRange = mergedRanges[i];
+                if (range.min < mergedRange.min && range.max < mergedRange.min)
+                {
+                    mergedRanges.Insert(i, range);
+                    isMerged = true;
+                    break;
+                }
+                if (range.min < mergedRange.min && range.max >= mergedRange.min)
+                {
+                    mergedRanges[i] = (range.min, Math.Max(range.max, mergedRange.max));
+                    isMerged = true;
+                    break;
+                }
+                if (range.min >= mergedRange.min && range.max <= mergedRange.max)
+                {
+                    isMerged = true;
+                    break;
+                }
+                if (range.min >= mergedRange.min && range.min <= mergedRange.max && range.max >= mergedRange.max)
+                {
+                    mergedRanges[i] = (mergedRange.min, range.max);
+                    isMerged = true;
+                    break;
+                }
+                if (range.min > mergedRange.max)
+                {
+                    continue;
+                }
+            }
+            if (!isMerged)
+            {
+                mergedRanges.Add(range);
+            }
+        }
+
+        foreach (var mergedRange in mergedRanges)
+        {
+            var count = mergedRange.max - mergedRange.min + 1;
+            answer += count;
+        }
 
         return answer;
     }
