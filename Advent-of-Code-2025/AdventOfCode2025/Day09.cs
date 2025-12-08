4d72369c7731dc245a6c7b08f54b4d9858751742
Mohit Makhija <mohit.makhija@avanade.com>
2025-12-08 10:28:46 +0100

    Day 09 Skeleton

---
+++ Advent-of-Code-2025/AdventOfCode2025/Day09.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2025;
+
+internal class Day09 : BaseDay<long>
+{
+    public override async Task<long> Puzzle1(string data)
+    {
+        long answer = 0;
+        var inputs = data.Split("\r\n");
+
+
+
+        return answer;
+    }
+
+    public override async Task<long> Puzzle2(string data)
+    {
+        long answer = 0;
+        var inputs = data.Split("\r\n");
+
+
+
+        return answer;
+    }
+}
--- Advent-of-Code-2025/AdventOfCode2025/Program.cs
+++ Advent-of-Code-2025/AdventOfCode2025/Program.cs
@@ -2,5 +2,5 @@
 
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-await new Day08().RunAsync();
+await new Day09().RunAsync();
 Console.ReadKey();
