4f8e776eb3b74d7b7f67d7eab395db7fe77f7804
Mohit Makhija <mohit@outlook.de>
2025-12-09 08:26:04 +0100

    Day 09 Puzzle 1

--- Advent-of-Code-2025/AdventOfCode2025/Day09.cs
+++ Advent-of-Code-2025/AdventOfCode2025/Day09.cs
@@ -10,8 +10,22 @@ internal class Day09 : BaseDay<long>
     {
         long answer = 0;
         var inputs = data.Split("\r\n");
-
-
+        var tiles = inputs.Select(x => x.Split(',')).Select(x => new Tile(int.Parse(x[0]), int.Parse(x[1]))).ToList();
+        long max = 0;
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            for (int j = 0; j < tiles.Count; j++)
+            {
+                var tile1 = tiles[i];
+                var tile2 = tiles[j];
+                var area = (Math.Abs(tile1.X - tile2.X) + 1) * (Math.Abs(tile1.Y - tile2.Y) + 1);
+                if (area >= max)
+                {
+                    max = area;
+                }
+            }
+        }
+        answer = max;
 
         return answer;
     }
@@ -25,4 +39,6 @@ internal class Day09 : BaseDay<long>
 
         return answer;
     }
+
+    public record Tile(long X, long Y);
 }
