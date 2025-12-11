366446baa02105255dc84a768d08b66fc620bb7b
Mohit Makhija <mohit@outlook.de>
2025-12-11 06:50:15 +0100

    Day 11 Puzzle 1

---
+++ Advent-of-Code-2025/AdventOfCode2025/Day11.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2025;
+
+internal class Day11 : BaseDay<long>
+{
+    public override async Task<long> Puzzle1(string data)
+    {
+        long answer = 0;
+        var inputs = data.Split("\r\n");
+        Dictionary<string, HashSet<string>> connections = [];
+        foreach ( var input in inputs )
+        {
+            var split = input.Split(": ");
+            connections.Add(split[0], [.. split[1].Split(' ')]);
+        }
+
+        var initial = "you";
+        var target = "out";
+        var queue = new Queue<string>();
+        queue.Enqueue(initial);
+        while (queue.Count > 0)
+        {
+            var input = queue.Dequeue();
+            if (input == target)
+            {
+                answer++;
+                continue;
+            }
+            var paths = connections[input];
+            foreach (var path in paths)
+            {
+                queue.Enqueue(path);
+            }
+        }
+        return answer;
+    }
+
+    public override async Task<long> Puzzle2(string data)
+    {
+        long answer = 0;
+        var inputs = data.Split("\r\n");
+
+
+
+        return answer;
+    }
+}
