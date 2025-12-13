8c35b78e73e6474e4384d211efeb0165e629e764
Mohit Makhija <mohit@outlook.de>
2025-12-13 18:30:48 +0100

    Day 11 Puzzle 2

--- Advent-of-Code-2025/AdventOfCode2025/BaseDay.cs
+++ Advent-of-Code-2025/AdventOfCode2025/BaseDay.cs
@@ -2,10 +2,11 @@
 
 namespace AdventOfCode2025;
 
-internal abstract class BaseDay<T>
+internal abstract class BaseDay<T>(bool useExample2 = false)
 {
     private string Input { get; set; } = File.ReadAllText("input.txt");
     private string ExampleInput { get; set; } = File.ReadAllText("example.txt");
+    private string ExampleInput2 { get; set; } = File.ReadAllText("example2.txt");
 
     public abstract Task<T> Puzzle1(string data);
     public abstract Task<T> Puzzle2(string data);
@@ -22,7 +23,7 @@ internal abstract class BaseDay<T>
         Stopwatch sw = Stopwatch.StartNew();
         Console.WriteLine($"Answer 1: {await Puzzle1(ExampleInput)} (Time: {sw})");
         sw.Restart();
-        Console.WriteLine($"Answer 2: {await Puzzle2(ExampleInput)} (Time: {sw})");
+        Console.WriteLine($"Answer 2: {await Puzzle2(useExample2 ? ExampleInput2 : ExampleInput)} (Time: {sw})");
         Console.WriteLine("--------------------------");
         Console.WriteLine("Puzzle Results:");
         sw.Restart();
